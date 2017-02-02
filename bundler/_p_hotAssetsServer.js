de519c4a9d05eef9ba5bac8abb9e7dbb921ba5bb
erwanledoux <lawanledoux@gmail.com>
2017-02-02 17:06:19 +0100

    cleant logs to display in webpack dev server

--- bundler/_p_hotAssetsServer.js
+++ bundler/_p_hotAssetsServer.js
@@ -12,10 +12,24 @@ new WebpackDevServer(
     contentBase: 'backend/servers/$[frontend.serverName]/app/templates/',
     hot: true,
     historyApiFallback: true,
-    publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+
+    // provide less noisy output from webpack
+    quiet: false,
+    noInfo: false,
+    stats: {
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
+    }
   }
 ).listen(PORT, HOST, function (err, result) {
   if (err) {
     return console.log(err)
   }
+  console.log(`You hot server is available here http://${HOST}:${PORT}`)
 })
